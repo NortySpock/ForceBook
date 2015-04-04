72ccb1eb1d25f3baedd3d970711849fedca9b8a4
Jason W. Thompson <jason@jasonandmary.com>
2015-04-04 16:56:10 -0500

    Updating stubs.

--- fb-person-datasource-mock/src/main/java/com/forcebook/person/datasource/mock/MockPersonDataSource.java
+++ fb-person-datasource-mock/src/main/java/com/forcebook/person/datasource/mock/MockPersonDataSource.java
@@ -2,6 +2,7 @@ package com.forcebook.person.datasource.mock;
 
 import com.forcebook.person.biz.Person;
 import com.forcebook.person.datasource.PersonDataSource;
+import com.forcebook.person.datasource.PersonMetaData;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,12 +21,6 @@ public class MockPersonDataSource implements PersonDataSource
     }
 
     @Override
-    public void retrievePersonByUID(String uid)
-    {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
     public void updatePerson(Person person)
     {
         throw new UnsupportedOperationException("Not supported yet.");
@@ -39,6 +34,18 @@ public class MockPersonDataSource implements PersonDataSource
 
     @Override
     public void restorePersonByUID(String uid) throws UnsupportedOperationException
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Person retrievePersonByUID(String uid)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public PersonMetaData retrievePersonMetaDataByUID(String UID)
     {
         throw new UnsupportedOperationException("Not supported yet.");
     }
