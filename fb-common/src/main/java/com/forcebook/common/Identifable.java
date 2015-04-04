e6a27ed422fec99cbdb75a4c95bcc33a6aa0e09d
Jason W. Thompson <jason@jasonandmary.com>
2015-04-04 15:58:31 -0500

    Added Identifiable.

---
+++ fb-common/src/main/java/com/forcebook/common/Identifable.java
@@ -0,0 +1,14 @@
+package com.forcebook.common;
+
+/**
+ * Indicates that an object can be identified uniquely
+ * @author Jason W. Thompson - https://google.com/+JasonWThompson_SoftwareDeveloper
+ */
+public interface Identifable
+{
+    /**
+     * The unique identifier of this object.
+     * @return The UID
+     */
+    String getUID();
+}
