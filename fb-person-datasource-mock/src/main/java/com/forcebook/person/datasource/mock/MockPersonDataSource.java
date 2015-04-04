cdc091c5b04b3e054df2674e0a1ad3f6a8c77d53
Jason W. Thompson <jason@jasonandmary.com>
2015-04-04 16:36:36 -0500

    Created project mock person datasource. Added mock implementation of person data source with stubs.

---
+++ fb-person-datasource-mock/src/main/java/com/forcebook/person/datasource/mock/MockPersonDataSource.java
@@ -0,0 +1,45 @@
+package com.forcebook.person.datasource.mock;
+
+import com.forcebook.person.biz.Person;
+import com.forcebook.person.datasource.PersonDataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A Mock implementation of a {@link PersonDataSource} that holds persons in memory.
+ * @author Jason W. Thompson - https://google.com/+JasonWThompson_SoftwareDeveloper
+ */
+public class MockPersonDataSource implements PersonDataSource
+{
+    private static Map<String, Person> personsByUID = new HashMap<>();
+    
+    @Override
+    public void createPerson(Person person)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void retrievePersonByUID(String uid)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void updatePerson(Person person)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void dropPersonByUID(String uid)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void restorePersonByUID(String uid) throws UnsupportedOperationException
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
