71a75acfad365a112214628549ee095b0be65e26
Jason W. Thompson <jason@jasonandmary.com>
2015-04-11 09:56:55 -0500

    Correcting the Name javadoc. Correcting the PersonManager.storePerson logic.

--- fb-person-biz/src/main/java/com/forcebook/person/biz/Name.java
+++ fb-person-biz/src/main/java/com/forcebook/person/biz/Name.java
@@ -11,31 +11,31 @@ import java.util.Optional;
 public interface Name extends Identifable
 {
     /**
-     * Gets the optional prefix of the {@code Name} such as Dr., Mr., Mrs., etc...
+     * The optional prefix of the {@code Name} such as Dr., Mr., Mrs., etc...
      * @return The name prefix
      */
     Optional<String> getPrefix();
     
     /**
-     * Gets the optional given name. In some cultures, this is known as a "first name"
+     * The optional given name. In some cultures, this is known as a "first name"
      * @return The given name.
      */
     Optional<String> getGiven();
 
     /**
-     * Gets the middle names. This {@code Collection} may be empty.
+     * The middle names. This {@code Collection} may be empty.
      * @return The middle name.
      */
     List<String> getMiddle();
 
     /**
-     * Gets the family name. In some cultures this is known as a last name.
+     * The family name. In some cultures this is known as a last name.
      * @return The family name.
      */
     String getFamily();
     
     /**
-     * Gets the name suffix such as Jr., III, etc...
+     * The name suffix such as Jr., III, etc...
      * @return The name suffix
      */
     Optional<String> getSuffix();
