b930728caba7846b39e9d44c5225656051139821
David Norton <david.k.norton@gmail.com>
2015-05-07 07:39:42 -0500

    should be the last round of fixups from latest review of pullrequest #2

--- fb-person-datasource-mock/src/main/java/com/forcebook/person/datasource/mock/MockPersonDataSource.java
+++ fb-person-datasource-mock/src/main/java/com/forcebook/person/datasource/mock/MockPersonDataSource.java
@@ -1,17 +1,15 @@
 package com.forcebook.person.datasource.mock;
 
-import com.forcebook.person.biz.Person;
-import com.forcebook.person.datasource.PersonDataSource;
-import com.forcebook.person.datasource.PersonMetaData;
-
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import java.security.InvalidParameterException;
-import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+
+import com.forcebook.person.biz.Person;
+import com.forcebook.person.datasource.PersonDataSource;
+import com.forcebook.person.datasource.PersonMetaData;
 
 /**
  * A Mock implementation of a {@link PersonDataSource} that holds persons in
@@ -19,8 +17,7 @@ import java.util.UUID;
  *
  * @author Jason W. Thompson -
  *         https://google.com/+JasonWThompson_SoftwareDeveloper
- * @author David K. Norton
- *         david.k.norton@gmail.com
+ * @author David K. Norton david.k.norton@gmail.com
  */
 public class MockPersonDataSource implements PersonDataSource {
     private static final Map<String, Person> PERSONS_BY_UID = new HashMap<>();
@@ -28,58 +25,58 @@ public class MockPersonDataSource implements PersonDataSource {
     private static final Map<String, PersonMetaData> PERSON_METADATA_BY_UID = new HashMap<>();
     private static final Map<String, PersonMetaData> DELETED_PERSON_METADATA_BY_UID = new HashMap<>();
 
-    @Override
-    public String createPerson(Person person) {
+    @Override public String createPerson(final Person person) {
         checkNotNull(person, "The parameter person is null.");
         final String uid = UUID.randomUUID().toString();
-        SimplePerson myPerson = new SimplePerson();
+        final SimplePerson myPerson = new SimplePerson();
         myPerson.setUID(uid);
         myPerson.setName(person.getName());
         myPerson.setForceAlignment(person.getForceAlignment());
-        SimplePersonMetaData metadata = new SimplePersonMetaData();
+        final SimplePersonMetaData metadata = new SimplePersonMetaData();
 
         PERSONS_BY_UID.put(uid, myPerson);
         PERSON_METADATA_BY_UID.put(uid, metadata);
         return uid;
     }
 
-    @Override
-    public void updatePerson(Person person) {
+    @Override public void updatePerson(final Person person) {
         checkNotNull(person, "The parameter person is null.");
-        String personUID = person.getUID();
-        //need previous person to be able to check for a force alignment change
-        final Person existingPerson = Optional.ofNullable(PERSONS_BY_UID.get(person.getUID())).orElseThrow(() -> new IllegalArgumentException("Cannot update non-existent person identified by the UID ["+ personUID + "]"));
-
-
+        Optional.of(person.getName()).orElseThrow(() -> new IllegalArgumentException("Cannot update person if new person has no name"));
+        Optional.of(person.getForceAlignment()).orElseThrow(() -> new IllegalArgumentException("Cannot update person if new person has no ForceAlignment"));
+        final String personUID = person.getUID();
+        // need previous person to be able to check for a force alignment change
+        final Person existingPerson = Optional.ofNullable(PERSONS_BY_UID.get(person.getUID())).orElseThrow(() -> new IllegalArgumentException("Cannot update non-existent person identified by the UID ["
+                + personUID + "]"));
 
         PERSONS_BY_UID.put(person.getUID(), person);
 
-        SimplePersonMetaData metadata = new SimplePersonMetaData();
+        final SimplePersonMetaData metadata = new SimplePersonMetaData();
 
-        //if the force alignment status changed (null vs LIGHT vs DARK)
-        if(existingPerson.getForceAlignment().isPresent() !=  person.getForceAlignment().isPresent()
-                || existingPerson.getForceAlignment().get() != person.getForceAlignment().get()){
+        // if the force alignment status changed (LIGHT vs DARK)
+        if (existingPerson.getForceAlignment().isPresent() != person.getForceAlignment().isPresent()
+                || existingPerson.getForceAlignment().get() != person.getForceAlignment().get()) {
             metadata.setForceAlignmentUpdateTime();
+        } else {
+            // need to store previous metadata value
+            final PersonMetaData existingMetadata = Optional.ofNullable(PERSON_METADATA_BY_UID.get(person.getUID())).orElseThrow(() -> new IllegalArgumentException("Cannot update non-existant person metadata identified by the UID ["
+                    + personUID + "]"));
+            metadata.setForceAlignmentUpdateTime(existingMetadata.getForceAlignmentUpdateTime());
         }
         PERSON_METADATA_BY_UID.put(person.getUID(), metadata);
     }
 
-
-
-    @Override
-    public void dropPersonByUID(String uid) {
+    @Override public void dropPersonByUID(final String uid) {
         final Optional<Person> deletedPerson = Optional.ofNullable(PERSONS_BY_UID.get(uid));
         final Optional<PersonMetaData> deletedMetaData = Optional.ofNullable(PERSON_METADATA_BY_UID.get(uid));
         if (deletedPerson.isPresent() && deletedMetaData.isPresent()) {
             DELETED_PERSONS_BY_UID.put(uid, deletedPerson.get());
             PERSONS_BY_UID.remove(uid);
-            DELETED_PERSON_METADATA_BY_UID.put(uid,deletedMetaData.get());
+            DELETED_PERSON_METADATA_BY_UID.put(uid, deletedMetaData.get());
             PERSON_METADATA_BY_UID.remove(uid);
         }
     }
 
-    @Override
-    public void restorePersonByUID(String uid) {
+    @Override public void restorePersonByUID(final String uid) {
         final Optional<Person> deletedPerson = Optional.ofNullable(DELETED_PERSONS_BY_UID.get(uid));
         final Optional<PersonMetaData> deletedMetaData = Optional.ofNullable(PERSON_METADATA_BY_UID.get(uid));
         if (deletedPerson.isPresent() && deletedMetaData.isPresent()) {
@@ -90,13 +87,12 @@ public class MockPersonDataSource implements PersonDataSource {
         }
     }
 
-    @Override
-    public Optional<Person> retrievePersonByUID(String uid) {
+    @Override public Optional<Person> retrievePersonByUID(final String uid) {
         return Optional.ofNullable(PERSONS_BY_UID.get(uid));
     }
 
-    @Override
-    public Optional<PersonMetaData> retrievePersonMetaDataByUID(String uid) {
+    @Override public Optional<PersonMetaData> retrievePersonMetaDataByUID(
+            final String uid) {
         return Optional.ofNullable(PERSON_METADATA_BY_UID.get(uid));
     }
 
--- fb-person-datasource-mock/src/main/java/com/forcebook/person/datasource/mock/SimplePersonMetaData.java
+++ fb-person-datasource-mock/src/main/java/com/forcebook/person/datasource/mock/SimplePersonMetaData.java
@@ -4,30 +4,32 @@ import java.time.Instant;
 
 import com.forcebook.person.datasource.PersonMetaData;
 
-public class SimplePersonMetaData implements PersonMetaData{
-    private Instant updateTime;
+public class SimplePersonMetaData implements PersonMetaData {
+    private final Instant updateTime;
     private Instant updateForceAlignmentTime;
 
     /**
      * Creates a {@code PersonMetaData} object for UID
-     * @param uid -- UID to attach meta data to
      */
     public SimplePersonMetaData() {
         updateTime = Instant.now();
     }
 
-    @Override
-    public Instant getUpdateTime() {
+    @Override public Instant getUpdateTime() {
         return updateTime;
     }
 
-    @Override
-    public Instant getForceAlignmentUpdateTime() {
+    @Override public Instant getForceAlignmentUpdateTime() {
         return updateForceAlignmentTime;
     }
 
     public void setForceAlignmentUpdateTime() {
         updateForceAlignmentTime = updateTime;
+    }
+
+    public void setForceAlignmentUpdateTime(
+            final Instant updateForceAlignmentTime) {
+        this.updateForceAlignmentTime = updateForceAlignmentTime;
     }
 
 }
